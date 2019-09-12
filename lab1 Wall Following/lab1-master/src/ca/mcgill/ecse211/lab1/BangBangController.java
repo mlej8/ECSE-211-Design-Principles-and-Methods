e9804a75c0d8805bdb11ba8b093e3ab612db7497
Yilin Jiang <yilin.jiang@mail.mcgill.ca>
2019-09-12 11:17:48 +0800

    Initial Version BangBang

--- lab1 Wall Following/lab1-master/src/ca/mcgill/ecse211/lab1/BangBangController.java
+++ lab1 Wall Following/lab1-master/src/ca/mcgill/ecse211/lab1/BangBangController.java
@@ -3,6 +3,11 @@ package ca.mcgill.ecse211.lab1;
 import static ca.mcgill.ecse211.lab1.Resources.*;
 
 public class BangBangController extends UltrasonicController {
+  private final int bandCenter=30;
+  private final int bandwidth=3;
+  private final int MOTOR_LOW;
+  private final int MOTOR_HIGH;
+  private final int DELTA_SPEED=MOTOR_HIGH-MOTOR_LOW;
 
   public BangBangController() {
     LEFT_MOTOR.setSpeed(MOTOR_HIGH); // Start robot moving forward
@@ -13,8 +18,21 @@ public class BangBangController extends UltrasonicController {
 
   @Override
   public void processUSData(int distance) {
-    filter(distance);
-
+     filter(distance);
+     int offset = bandCenter-distance;
+     if(Math.abs(offset)>bandwidth){
+       if(offset>0){
+         LEFT_MOTOR.setSpeed(MOTOR_HIGH+DELTA_SPEED);
+         RIGHT_MOTOR.setSpeed(0);
+         LEFT_MOTOR.forward();
+         RIGHT_MOTOR.forward();
+       }else{
+         LEFT_MOTOR.setSpeed(0);
+         RIGHT_MOTOR.setSpeed(MOTOR_HIGH+DELTA_SPEED));
+         LEFT_MOTOR.forward();
+         RIGHT_MOTOR.forward();
+       }
+     }
     // TODO: process a movement based on the us distance passed in (BANG-BANG style)
   }
 
